22f9669c1ae6d00ff71fe296ca7d21a7836edbc7
dimitribativa <26858677-dimitribativa@users.noreply.replit.com>
2025-02-25 00:48:31 +0000

    User checkpoint: Implement initial website design for Tropicália, including homepage, about section, and contact form.  Added accessibility features and styling.

---
+++ script.js
@@ -0,0 +1,5 @@
+const tituloMovimento = document.getElementById('titulo-movimento');
+tituloMovimento.textContent = "O que foi a Neighbourhood";
+
+const paragrafoDescreveMovimento = document.getElementById('paragrafo-descreve-movimento');
+paragrafoDescreveMovimento.textContent = ""
